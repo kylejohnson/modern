c21a7b01373e390865b0e6ca6222379c5d1b6f6c
Kyle Johnson <kjohnson@fixertec.net>
2010-04-08 14:37:41 -0400

    Got event filtering and pagination working with multiple monitors!!

--- js/jquery_pagination.js
+++ js/jquery_pagination.js
@@ -10,14 +10,14 @@ var url = location.search;
          $("#loading").fadeOut('slow');
 	};
 
-	function Build_Pagination() {
+	function Build_Pagination(query) {
          Hide_Load();
 
 	 $("#pagination li").click(function() {
            Display_Load();
 	  var pageNum = this.id;
           var monitorName = $("#inptMonitorName").attr("value");
-	  $("#events").load("/skins/new/views/pagination_data.php?filter[terms][0][attr]=MonitorName&filter[terms][0][op]==&filter[terms][0][val]=" + monitorName + "&page=" + pageNum, function () { Build_Pagination() });
+	  $("#events").load("/skins/new/views/pagination_data.php?&page=" + pageNum + query, function () { Build_Pagination() });
 	});
 
         };
@@ -27,8 +27,30 @@ var url = location.search;
 
 
  $('#sidebarHistory input').change(function() {
+
+  var allVals = []; //Make the array
+
+  $("#sidebarHistory input:checked").each(function() { //For each checked box
+   allVals.push(this.id); //Push the checkbox id into the array
+  });
+
+  var x = allVals.length; //Get the number of checked boxes
+  var query = "";
+  for (i=0;i<x;i++) { //For each checked box
+   if (i>0){
+    query += "&filter[terms][" + i + "][cnj]=or&filter[terms][" + i + "][attr]=MonitorName&filter[terms][" + i + "][op]==&filter[terms][" + i + "][val]=" + allVals[i];
+   } else {
+    query += "&filter[terms][" + i + "][attr]=MonitorName&filter[terms][" + i + "][op]==&filter[terms][" + i + "][val]=" + allVals[i];
+   }
+  };
+
   $("#inptMonitorName").attr('value', this.id);
+
   Display_Load();
-  $("#events").load("/skins/new/views/pagination_data.php?page=1&filter[terms][0][attr]=MonitorName&filter[terms][0][op]==&filter[terms][0][val]=" + this.id, function() { Build_Pagination()});
+
+//  $("#events").load("/skins/new/views/pagination_data.php?page=1&filter[terms][0][attr]=MonitorName&filter[terms][0][op]==&filter[terms][0][val]=" + this.id, function() { Build_Pagination()});
+  $("#events").load("/skins/new/views/pagination_data.php?page=1" + query, function() { Build_Pagination()});
+
  });
+
 });
