423d70f82d4fa519f8f56853a90a112d199c3c7f
Kyle Johnson <kjohnson@fixertec.net>
2010-04-09 09:53:48 -0400

    Cleaned up code, added comments and made filtering of events work correctly.
     * Used the value of a hidden input for the initially-selected monitor
     * Used that value for inital (before any additional filtering) pagination

--- js/jquery_pagination.js
+++ js/jquery_pagination.js
@@ -1,56 +1,47 @@
 $(document).ready(function(){
+ url = location.search;
+ query = "";
+ function Display_Load() {
+  $("#loading").fadeIn(900,0);
+  $("#loading").html("<img src='/skins/new/graphics/bigLoader.gif' />");
+ };
 
-var url = location.search;
-	function Display_Load() {
-	 $("#loading").fadeIn(900,0);
-         $("#loading").html("<img src='/skins/new/graphics/bigLoader.gif' />");
-	};
+ function Hide_Load() {
+  $("#loading").fadeOut('slow');
+ };
 
-	function Hide_Load() {
-         $("#loading").fadeOut('slow');
-	};
+ function Build_Pagination() {
+  Hide_Load();  //Hide spinner
+  var monitorName = $("#inptMonitorName").attr("value"); // Get the currently selected monitor.  This is only needed for the first page load, before any filters are set.
+  $("#pagination li").click(function() { //If page is changed
+   Display_Load(); //Show spinner
+   var pageNum = this.id; //Set page number
+   if (!(query == "")) {
+    $("#events").load("/skins/new/views/pagination_data.php?page=" + pageNum + query, function () { Build_Pagination() }); //Load data to page then rebuild Build_Pagination function
+   } else {
+    $("#events").load("/skins/new/views/pagination_data.php?page=" + pageNum + "&filter[terms][0][attr]=MonitorName&filter[terms][0][op]==&filter[terms][0][val]=" + monitorName, function () { Build_Pagination() }); //Load data to page then rebuild Build_Pagination function
+   }
+  });
+ };
 
-	function Build_Pagination(query) {
-         Hide_Load();
+ Display_Load(); //First thing that happens - display spinner
+ $("#events").load("/skins/new/views/pagination_data.php" + url, function() { Build_Pagination() }); //Second, load data into #events then build Build_Pagination function
 
-	 $("#pagination li").click(function() {
-           Display_Load();
-	  var pageNum = this.id;
-          var monitorName = $("#inptMonitorName").attr("value");
-	  $("#events").load("/skins/new/views/pagination_data.php?&page=" + pageNum + query, function () { Build_Pagination() });
-	});
-
-        };
-
-	Display_Load();
-	 $("#events").load("/skins/new/views/pagination_data.php" + url, function() { Build_Pagination() });
-
-
- $('#sidebarHistory input').change(function() {
-
+ $('#sidebarHistory input').change(function() { //When a checkbox is checked
   var allVals = []; //Make the array
-
   $("#sidebarHistory input:checked").each(function() { //For each checked box
    allVals.push(this.id); //Push the checkbox id into the array
   });
-
   var x = allVals.length; //Get the number of checked boxes
-  var query = "";
   for (i=0;i<x;i++) { //For each checked box
-   if (i>0){
-    query += "&filter[terms][" + i + "][cnj]=or&filter[terms][" + i + "][attr]=MonitorName&filter[terms][" + i + "][op]==&filter[terms][" + i + "][val]=" + allVals[i];
+   if (i>0){ //If more than 1 checked box
+    query += "&filter[terms][" + i + "][cnj]=or&filter[terms][" + i + "][attr]=MonitorName&filter[terms][" + i + "][op]==&filter[terms][" + i + "][val]=" + allVals[i]; // add "or"
    } else {
-    query += "&filter[terms][" + i + "][attr]=MonitorName&filter[terms][" + i + "][op]==&filter[terms][" + i + "][val]=" + allVals[i];
+    query += "&filter[terms][" + i + "][attr]=MonitorName&filter[terms][" + i + "][op]==&filter[terms][" + i + "][val]=" + allVals[i]; // Add nothing
    }
   };
-
-  $("#inptMonitorName").attr('value', this.id);
-
-  Display_Load();
-
-//  $("#events").load("/skins/new/views/pagination_data.php?page=1&filter[terms][0][attr]=MonitorName&filter[terms][0][op]==&filter[terms][0][val]=" + this.id, function() { Build_Pagination()});
-  $("#events").load("/skins/new/views/pagination_data.php?page=1" + query, function() { Build_Pagination()});
-
+  monitorName = this.id
+  Display_Load(); //Display spinner
+  $("#events").load("/skins/new/views/pagination_data.php?page=1" + query, function() { Build_Pagination()}); // Load data into #events
  });
-
 });
