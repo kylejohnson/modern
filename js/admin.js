195f97d240396296b74c8eb119d1aacaef310c2c
Kyle Johnson <kjohnson@fixertec.net>
2010-04-14 12:08:55 -0400

    Moved admin.php javascript from functions.php to admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
+$(function() {
+  $("#content").load("/skins/new/views/adminMonitors.php", function() { Load_Monitors() });
+
+ function Load_Monitors() {
+  $("#delMonitor").click(function() {
+   if (confirm('Are you sure you want to delete the selected monitors and all associated events?')) {
+    $("#tblMonitors input:checked").each(function() {
+     var MonitorId = $(this).attr("value");
+     $.post("skins/new/includes/deleteMonitor.php?MonitorId="+ MonitorId);
+    });
+   }
+   $("#content").load("/skins/new/views/adminMonitors.php");
+  });
  $("#addMonitor").colorbox({iframe:true, innerWidth:340, innerHeight:400});
+ }
 });
