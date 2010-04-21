5746c000aefb71f2289cf107894e8375067ecfd7
Kyle Johnson <kjohnson@fixertec.net>
2010-04-21 14:52:28 -0400

    Added an extra div around the images, and a div for the spinner

--- views/monitors.php
+++ views/monitors.php
@@ -68,8 +68,11 @@ foreach( $displayMonitors as $monitor ){
  }
 ?>
 <li id="monitor_<?php echo $monitor['Id'] ?>">
+ <div class="mon">
  <a href="/?view=events&page=1&filter[terms][0][attr]=MonitorId&filter[terms][0][op]==&filter[terms][0][val]=<?php echo $monitor['Id'] ?>" >
   <?php outputImageStill( "liveStream", $streamSrc, reScale( $monitor['Width'], $scale ), reScale( $monitor['Height'], $scale ), $monitor['Name'] ); ?>
  </a>
+ </div>
+ <div class="spinner"></div>
 </li>
 <?php } ?>
