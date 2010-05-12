b6a897be3eac3e24980b32868ee649428194db2e
Kyle Johnson <kjohnson@fixertec.net>
2010-05-12 14:57:53 -0400

    Changed the way that the images are generated and the gallery built.

--- views/stills.php
+++ views/stills.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php
-include("../../../includes/config.php");
-$cwd = ZM_PATH_WEB; # Get the current working directory (root path to zm install)
-$path = $_REQUEST['path'];
-$files = scandir($cwd . '/' . $path);
-array_shift($files); # Delete first 3 entires (.., . and .file);
-array_shift($files);
-array_shift($files);
-$paths = array();
-foreach ($files as $file){ # For each file, push the path + file into paths
- if (preg_match("/capture/i", $file)){
-  array_push($paths, $path . $file);
- }
-};
-?>
 <html>
 <head>
  <title>Stills</title>
  <script type="text/javascript" src="../js/jquery-1.4.2.min.js"></script>
  <script type="text/javascript" src="../js/jquery-ui-1.8.custom.min.js"></script>
  <script type="text/javascript" src="../js/jquery.ad-gallery.js"></script>
- <link rel="stylesheet" href="skins/new/css/jquery.ad-gallery.css" type="text/css"/>
+ <link rel="stylesheet" href="../css/jquery.ad-gallery.css" type="text/css"/>
  <script type="text/javascript">
   $(function(){
+   var imgs = new Array();
+   var qs = window.location.search.substring(1);
+   var wtf = qs.split("=");
+   var path = wtf[1];
+   
+   $.post("../includes/getFiles.php?path=" + path, function(data){
+    imgs = data.split(" ");
+    var x = imgs.length;
+    for (var i=0;i<x;i++){
+     $(".ad-thumb-list").append('<li><a href="/'+imgs[i]+'">'+'<img src="/'+imgs[i]+'"/>'+'</a></li>');
+    };
+    $(".ad-thumb-list img").attr('width', '250');
+    build_gallery();
+   });
+ 
+   function build_gallery(){
+
    var galleries = $('.ad-gallery').adGallery();
+   };
   });
  </script>
 </head>
 <body>
-<div class="ad-gallery">
+<div id="gallery" class="ad-gallery">
   <div class="ad-image-wrapper">
   </div>
   <div class="ad-controls">
@@ -36,16 +39,6 @@ foreach ($files as $file){ # For each file, push the path + file into paths
   <div class="ad-nav">
     <div class="ad-thumbs">
       <ul class="ad-thumb-list">
-        <li>
-          <a href="images/1.jpg">
-            <img src="images/thumbs/t1.jpg" title="Title for 1.jpg">
-          </a>
-        </li>
-        <li>
-          <a href="images/2.jpg">
-            <img src="images/thumbs/t2.jpg" longdesc="Description of the image 2.jpg">
-          </a>
-        </li>
       </ul>
     </div>
   </div>
