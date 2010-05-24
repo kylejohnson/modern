9a1655e7525713b8d27bc2c99d58d2014bf6e455
Kyle Johnson <kjohnson@fixertec.net>
2010-05-24 15:56:22 -0400

    Moved version next to state

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,9 @@
 <div id="footer">
       <div class="left"><?= makePopupLink( '?view=groups', 'zmGroups', 'groups', sprintf( $CLANG['MonitorCount'], count($displayMonitors), zmVlang( $VLANG['Monitor'], count($displayMonitors) ) ).($group?' ('.$group['Name'].')':''), canView( 'System' ) ); ?></div>
- <div class="center"><?= makePopupLink( '?view=state', 'zmState', 'state', $status, canEdit( 'System' ) ) ?></div>
+ <div class="center">
+  <?= makePopupLink( '?view=state', 'zmState', 'state', $status, canEdit( 'System' ) ) ?> <span>-</span> 
+  <?= makePopupLink( '?view=version', 'zmVersion', 'version', "v".ZM_VERSION, canEdit( 'System' ) ) ?>
+ </div>
  <div class="right">
   <?php
    if ( ZM_OPT_USE_AUTH ){
@@ -12,7 +15,6 @@
    <?= $SLANG['ConfiguredFor'] ?>
   <?php }?>
   <?= makePopupLink( '?view=bandwidth', 'zmBandwidth', 'bandwidth', $bwArray[$_COOKIE['zmBandwidth']], ($user && $user['MaxBandwidth'] != 'low' ) ) ?> <?= $SLANG['Bandwidth'] ?>
-   <?= makePopupLink( '?view=version', 'zmVersion', 'version', "v".ZM_VERSION, canEdit( 'System' ) ) ?>
  </div>
 </div>
     </div>
