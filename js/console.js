88dbf10b0b58eee78d3c49a7aeb4d8e232cfa7b5
Kyle Johnson <kjohnson@fixertec.net>
2010-07-28 08:54:20 -0400

    Added code for adding, deleting and loading tabs (groups).

--- js/console.js
+++ js/console.js
@@ -3,11 +3,24 @@
    refresh = (refresh * 1000);
    $("#footer a").colorbox({iframe:true, width:'25%', height:'25%'});
    $("#monitors").load("skins/new/views/monitors.php", function(){post_load()});
-   $("#tabs").tabs();
+   $("#tabs").tabs({
 
-   $("#add_tab").click(function() {
-   });
-   
+			tabTemplate: '<li><a href="#{href}">#{label}</a> <span class="ui-icon ui-icon-close">Remove Tab</span></li>'
+
+});
+   $("#add_widget").button();
+   load_tabs();
+
+   function load_tabs() {
+    $.post("skins/new/includes/updateGroups.php?action=select", function(data) {
+     saved_tabs = data.split(",");
+     saved_tabs.pop();
+     var x = saved_tabs.length;
+     for (var i=0;i<x;i++){
+      $("#tabs").tabs('add', '#tabs-'+saved_tabs[i], saved_tabs[i]);
+     }
+    });
+   };
 
    function post_load() {
     $("a[rel='monitor']").colorbox({
@@ -22,6 +35,65 @@
     $(".minimize").click(function() {
      $(this).parent().parent().find('.mon').toggle('blind');
     });
+
+    var $tab_title_input = $('#tab_title'), $tab_content_input = $('#tab_content');
+
+		// modal dialog init: custom buttons and a "close" callback reseting the form inside
+		var $dialog = $('#dialog').dialog({
+			autoOpen: false,
+			modal: true,
+			buttons: {
+				'Add': function() {
+					addTab();
+					$(this).dialog('close');
+				},
+				'Cancel': function() {
+					$(this).dialog('close');
+				}
+			},
+			open: function() {
+				$tab_title_input.focus();
+			}
+		});
+
+		// addTab form: calls addTab function on submit and closes the dialog
+		var $form = $('form',$dialog).submit(function() {
+			addTab();
+			$dialog.dialog('close');
+			return false;
+		});
+
+		// actual addTab function: adds new tab using the title input from the form above
+		function addTab() {
+			tab_title = $tab_title_input.val();
+			postIt();
+		}
+
+function postIt() {
+var arysel  = [];
+$("#selMonitors :selected").each(function(i, selected) {
+ arysel[i] = $(selected).val();
+});
+var mids = arysel.toString();
+$.post("skins/new/includes/updateGroups.php?groupName=" + tab_title + "&mids=" + mids + "&action=insert");
+$("#tabs").tabs('add', '#tabs-'+tab_title, tab_title);
+}
+
+		// addTab button: just opens the dialog
+		$('#add_tab')
+			.button()
+			.click(function() {
+				$dialog.dialog('open');
+			});
+
+		// close icon: removing the tab on click
+		// note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
+		$('#tabs span.ui-icon-close').click(function() {
+			var index = $('li',$("#tabs")).index($(this).parent());
+			$.post("skins/new/includes/updateGroups.php?action=delete&groupName=" + $(this).parent().find('a').text());
+			$("#tabs").tabs('remove', index);
+		});
+
    }
 
   setInterval(function() {
