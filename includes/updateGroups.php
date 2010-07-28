7a5c9bf6432239b4d23847e7068734570a5e7969
Kyle Johnson <kjohnson@fixertec.net>
2010-07-28 08:01:34 -0400

    Added comments and fixed code to correctly print out an array

--- includes/updateGroups.php
+++ includes/updateGroups.php
@@ -4,18 +4,23 @@ $groupName 	= $_REQUEST['groupName'];
 $mids		= $_REQUEST['mids'];
 $action		= $_REQUEST['action'];
 
-if ($action == "insert") {
+if ($action == "insert") { # Insert a new group
  $existing = mysql_query("select Id from Groups where Name = '" . $groupName . "'");
- if (!$row = mysql_fetch_array($existing)) {
+ if (!$row = mysql_fetch_array($existing)) { # Only if the group does not already exist
   $query = "insert into Groups (Name, MonitorIds) VALUES ('" . $groupName . "', '" . $mids ."')";
  }
-} elseif ($action == "update") {
+} elseif ($action == "update") { # Update a group (Currently name only)
  $query = "update Groups set Name = '" . $groupName . "'";
-} elseif ($action == "delete") {
+} elseif ($action == "delete") { # Delete a group
  $query = "delete from Groups where Name = '" . $groupName . "'";
+} elseif ($action == "select") { # Select all the tabs to be loaded into the Daskboard
+ $query = "select Name from Groups";
 } else {
  die('Invalid Action!');
 }
 
-mysql_query($query) or die('Error, query failed!');
+$result = mysql_query($query) or die('Error, query failed!');
+while($row = mysql_fetch_array($result)){
+ echo $row['Name'] . ',';
+}
 ?>
