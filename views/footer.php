cce57c27c33511031ad7725bc751ffeaa3515cdc
Kyle Johnson <kjohnson@fixertec.net>
2010-08-20 10:09:28 -0400

    Removed extra <div> tag

--- views/footer.php
+++ views/footer.php
@@ -17,7 +17,6 @@
   <?= makePopupLink( '?view=bandwidth', 'zmBandwidth', 'bandwidth', $bwArray[$_COOKIE['zmBandwidth']], ($user && $user['MaxBandwidth'] != 'low' ) ) ?> <?= $SLANG['Bandwidth'] ?>
  </div>
 </div>
-    </div>
 </body>
 </html>
 
