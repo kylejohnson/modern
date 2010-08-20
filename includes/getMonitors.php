808eae066de599bee0140a61a1e295be0cdf3d2f
Kyle Johnson <kjohnson@fixertec.net>
2010-08-20 11:20:58 -0400

    Fixed incorrect data being echoed

--- includes/getMonitors.php
+++ includes/getMonitors.php
@@ -1,8 +1,8 @@
 <?php
  require("../../../includes/config.php");
- $query = "select Id, Name from Monitors order by Name";
+ $query = "select Name from Monitors order by Name";
  $result = mysql_query($query) or die('Error, selecting monitors failed.');
  while ($row = mysql_fetch_array($result)){
-  echo $row['Id'] . ' ' . $row['Name'] . ',';
+  echo $row['Name'] . ',';
  }
 ?>
