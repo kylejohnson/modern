40d63887ff54b4169dff39646cf55cb29828c2a6
Kyle Johnson <kjohnson@fixertec.net>
2010-08-20 10:32:58 -0400

    Initial commit of code for events page.
     * Setting up tabs
     * Displaying event thumbs
     * Infinite scroll
     * User interaction

---
+++ js/events.js
@@ -0,0 +1,23 @@
+$(function() {
+ $("#tabs").tabs(); // Init tabs
+
+ // Add list of monitors
+ add_monitors();
+
+ // Load all events for selected monitor 
+
+
+ //FUNCTIONS//
+ function add_monitors(){
+  $.post("skins/new/includes/getMonitors.php", function(data){
+   var monitors = data.split(","); // Put monitors into array
+   monitors.pop(); // Pop off last monitor (it is blank)
+   var x = monitors.length; // Number of monitors
+   for (var i=0;i<x;i++){
+    var monitor = monitors[i];
+    $("#tabs").tabs('add', monitor, monitor);
+   }
+  });
+ };
+ //FUNCTIONS//
+});
