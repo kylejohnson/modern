9eb373c2f2614de6ad90beb3dcd03404dead88d7
Kyle Johnson <kjohnson@fixertec.net>
2010-08-23 11:27:56 -0400

    Code updates
     * Added support for returning "pages" of data via $offset and $page
     * Removed the old $query
     * Added EventID as <div>s id tag

--- includes/getEvents.php
+++ includes/getEvents.php
@@ -3,14 +3,18 @@ require_once("../../../includes/config.php");
 require_once("../../../includes/functions.php");
 require_once("../../../includes/database.php");
 chdir(ZM_PATH_WEB);
- $MonitorName = $_REQUEST['MonitorName'];
-# $query = "select Id, Width, Height, Date(StartTime) as Date, Time(StartTime) as Time, Length from Events where MonitorId = 1 limit 25";
-$query = "select E.Id,E.MonitorId,M.Name As MonitorName,M.Width,M.Height,M.DefaultScale,Date(E.StartTime) as Date, Time(E.StartTime) as Time,E.Length from Monitors as M inner join Events as E on (M.Id = E.MonitorId) where (M.Name = '$MonitorName') limit 100";
+
+$MonitorName = $_REQUEST['MonitorName'];
+$page = $_REQUEST['page'];
+$offset = ($page * 50);
+
+$query = "select E.Id,E.MonitorId,M.Name As MonitorName,M.Width,M.Height,M.DefaultScale,Date(E.StartTime) as Date, Time(E.StartTime) as Time,E.Length from Monitors as M inner join Events as E on (M.Id = E.MonitorId) where (M.Name = '$MonitorName') limit $offset,50";
  $result = mysql_query($query) or die('Error, selecting monitors failed.');
- while ($event = mysql_fetch_array($result)){
+
+while ($event = mysql_fetch_array($result)){
  $thumbData = createListThumbnail($event);
 ?>
- <div class="thumb" style="float:left; padding:4px 0; width:175px; height:190px; ">
+ <div class="thumb" style="float:left; padding:4px 0; width:175px; height:190px;" id="<?=$event['Id']?>">
   <img src="<?= $thumbData['Path'] ?>" alt="<?= $event['Id'] ?> Thumbnail"/>
   <p>Date: <?=$event['Date'] ?></p>
   <p>Time: <?=$event['Time'] ?></p>
