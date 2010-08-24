c0087cfb3ab10425182f86e392093ba3ed6c035a
Kyle Johnson <kjohnson@fixertec.net>
2010-08-24 09:48:34 -0400

    Removed hardcoded CSS.  This will need to be set dynamically with JS.

--- includes/getEvents.php
+++ includes/getEvents.php
@@ -14,7 +14,7 @@ $query = "select E.Id,E.MonitorId,M.Name As MonitorName,M.Width,M.Height,M.Defau
 while ($event = mysql_fetch_array($result)){
  $thumbData = createListThumbnail($event);
 ?>
- <div class="thumb" style="float:left; padding:4px 0; width:175px; height:190px;" id="<?=$event['Id']?>">
+ <div class="thumb" id="<?=$event['Id']?>">
   <a class="event" href="?view=event&amp;eid=<?= $event['Id'] ?>"><img src="<?= $thumbData['Path'] ?>" alt="<?= $event['Id'] ?> Thumbnail"/></a>
   <p>Date: <?=$event['Date'] ?></p>
   <p>Time: <?=$event['Time'] ?></p>
