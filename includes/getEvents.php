17829a813008b354c3ff559e3b16c5ba08940c86
Kyle Johnson <kjohnson@fixertec.net>
2010-09-09 09:08:46 -0400

    Added a little navigation-aid (pagemark) to the events list to help give some context to the results

--- includes/getEvents.php
+++ includes/getEvents.php
@@ -7,10 +7,17 @@ chdir(ZM_PATH_WEB);
 $MonitorName = $_REQUEST['MonitorName'];
 $page = $_REQUEST['page'];
 $offset = ($page * 25);
+$count = $page * $offset;
 
 $query = "select E.Id,E.MonitorId,M.Name As MonitorName,M.Width,M.Height,M.DefaultScale,Date(E.StartTime) as Date, Time(E.StartTime) as Time,E.Length from Monitors as M inner join Events as E on (M.Id = E.MonitorId) where (M.Name = '$MonitorName') limit $offset,25";
  $result = mysql_query($query) or die('Error, selecting monitors failed.');
-
+ if ($page){
+?>
+ <div class="pagemark">
+  <p><?=$offset+1?> to <?=$offset+25?></p>
+ </div>
+<?php
+ }
 while ($event = mysql_fetch_array($result)){
  $thumbData = createListThumbnail($event);
 ?>
