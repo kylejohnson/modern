f9509cd2addfc6b1521d8b946da36b4f07adb374
Kyle Johnson <kjohnson@fixertec.net>
2010-09-09 08:46:35 -0400

    Changed return from 50 to 25 events

--- includes/getEvents.php
+++ includes/getEvents.php
@@ -8,7 +8,7 @@ $MonitorName = $_REQUEST['MonitorName'];
 $page = $_REQUEST['page'];
 $offset = ($page * 25);
 
-$query = "select E.Id,E.MonitorId,M.Name As MonitorName,M.Width,M.Height,M.DefaultScale,Date(E.StartTime) as Date, Time(E.StartTime) as Time,E.Length from Monitors as M inner join Events as E on (M.Id = E.MonitorId) where (M.Name = '$MonitorName') limit $offset,50";
+$query = "select E.Id,E.MonitorId,M.Name As MonitorName,M.Width,M.Height,M.DefaultScale,Date(E.StartTime) as Date, Time(E.StartTime) as Time,E.Length from Monitors as M inner join Events as E on (M.Id = E.MonitorId) where (M.Name = '$MonitorName') limit $offset,25";
  $result = mysql_query($query) or die('Error, selecting monitors failed.');
 
 while ($event = mysql_fetch_array($result)){
