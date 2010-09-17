fdf9613def0eda4f9688b1d0670b793a56f2caa0
Kyle Johnson <kjohnson@fixertec.net>
2010-09-17 09:06:29 -0400

    Reorganized the execution order of the code, and made some other changes
     * Minimizing and sorting in all tabs now works

--- js/console.js
+++ js/console.js
@@ -1,93 +1,110 @@
-  $(document).ready(function(){
-   var refresh = $("#inptRefresh").val();
-   refresh = (refresh * 1000);
-   $("#monitors").load("skins/new/views/monitors.php", function(){post_load()});
-   $("#tabs").tabs({
-    tabTemplate: '<li><a href="#{href}">#{label}</a> <span class="ui-icon ui-icon-close">Remove Tab</span></li>'
-   });
-   $("#add_widget").button();
-   load_tabs()
+$(document).ready(function(){
+ var refresh = $("#inptRefresh").val();
+ refresh = (refresh * 1000);
+ $("#add_widget").button();
 
-   function load_tabs() {
-    $.post("skins/new/includes/updateGroups.php?action=select", function(data) {
-     saved_tabs = data.split(",");
-     saved_tabs.pop();
-     var x = saved_tabs.length;
-     for (var i=0;i<x;i++){
-      var name = saved_tabs[i];
-      $("#tabs").tabs('add', 'skins/new/views/monitors.php?groupName='+name, name);
-     }
-    })};
+ $("#tabs").tabs({ // First, Initialize tabs and the tab template
+  tabTemplate: '<li><a href="#{href}">#{label}</a> <span class="ui-icon ui-icon-close">Remove Tab</span></li>',
+  load: function(event, ui){ // When the tab is loaded
+   add_features();  // Add features
+  },
+  select: function(event, ui){ // When a tab is selected
+   $("#monitors").remove(); // Remove old monitors 
+  }
+ });
+ load_tabs(); // Then load the tabs into #tabs
+ add_tab_click(); // Finally, setup the add tab feature
 
-   function post_load() {
-    $("a[rel='monitor']").colorbox({
-     iframe:true,
-     preloading:false,
-     current:'{current} of {total}',
-     width:'85%',
-     height:'95%'
-    });
-    
-    $(".minimize").click(function() {
-     $(this).parent().parent().parent().find('.mon').toggle('blind');
-    });
-
-
-    var $tab_title_input = $('#tab_title'), $tab_content_input = $('#tab_content');
-
-		// modal dialog init: custom buttons and a "close" callback reseting the form inside
-		var $dialog = $('#dialog').dialog({
-			autoOpen: false,
-			modal: true,
-			buttons: {
-				'Add': function() {
-					addTab();
-					$(this).dialog('close');
-				},
-				'Cancel': function() {
-					$(this).dialog('close');
-				}
-			},
-			open: function() {
-				$tab_title_input.focus();
-			}
-		});
-
-		// addTab form: calls addTab function on submit and closes the dialog
-		var $form = $('form',$dialog).submit(function() {
-			addTab();
-			$dialog.dialog('close');
-			return false;
-		});
-
-		// actual addTab function: adds new tab using the title input from the form above
-		function addTab() {
-		 tab_title = $tab_title_input.val(); // groupName
-		 var arysel  = []; // An array for the monitorIds
-		 $("#selMonitors :selected").each(function(i, selected) { // For each selected MonitorId
-		  arysel[i] = $(selected).val(); // Put it into the array
-		 });
-		 var mids = arysel.toString(); // Make a comman-separated list of the select MonitorsIds
-  		 $.post("skins/new/includes/updateGroups.php?groupName=" + tab_title + "&mids=" + mids + "&action=insert"); // Add the new Group
-                 $("#tabs").tabs('add', 'skins/new/views/monitors.php?groupName='+tab_title, tab_title); // Add the actual tab
-	        }
-
-		// addTab button: just opens the dialog
-		$('#add_tab')
-			.button()
-			.click(function() {
-				$dialog.dialog('open');
-			});
-
-		// close icon: removing the tab on click
-		// note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
-		$('#tabs span.ui-icon').click(function() {
-			var index = $('li',$("#tabs")).index($(this).parent());
-			$.post("skins/new/includes/updateGroups.php?action=delete&groupName=" + $(this).parent().find('a').text());
-			$("#tabs").tabs('remove', index);
-		});
-
+ function load_tabs() {
+  $.post("skins/new/includes/updateGroups.php?action=select", function(data) { // Load tabs (groups)
+   saved_tabs = data.split(",");
+   saved_tabs.pop();
+   var x = saved_tabs.length;
+   for (var i=0;i<x;i++){
+    var name = saved_tabs[i];
+    $("#tabs").tabs('add', 'skins/new/views/monitors.php?groupName='+name, name); // Add the tab
    }
+  });
+ };
+
+ function add_features() {
+  // This function is called whenever cameras are loaded (via the tab load event)
+  // It setups up Minimize, Sortable, Colorbox features for the newly-loaded monitors
+  $(".minimize").click(function() { // Minimize
+   $(this).parent().parent().parent().find('.mon').toggle('blind');
+  });
+
+  $("#monitors").sortable({ opacity: 0.6, cursor: 'move', update: function() { // Sortable
+   var order = $(this).sortable("serialize") + '&action=sequence';
+   $.post("skins/new/includes/updateSequence.php", order);
+  }});
+
+  $("a[rel='monitor']").colorbox({ // Colorbox
+   iframe:true,
+   preloading:false,
+   current:'{current} of {total}',
+   width:'85%',
+   height:'95%'
+  });
+ };  // END ADD_FEATURES //
+
+ function add_tab_click() { // Call this last
+  // This whole shabang is used for adding tabs.
+  // We get the tab title and contents, setup the dialog box, create the functions
+  // for adding the tab, deleting tab, closing and opening dialog box, etc.
+
+  var $tab_title_input = $('#tab_title'), $tab_content_input = $('#tab_content');
+
+  // modal dialog init: custom buttons and a "close" callback reseting the form inside
+  var $dialog = $('#dialog').dialog({
+   autoOpen: false,
+   modal: true,
+   buttons: {
+    'Add': function() {
+      addTab();
+      $(this).dialog('close');
+     },
+    'Cancel': function() {
+     $(this).dialog('close');
+    }
+   },
+   open: function() {
+    tab_title_input.focus();
+   }
+  }); // END DIALOG //
+
+  // addTab form: calls addTab function on submit and closes the dialog
+  var $form = $('form',$dialog).submit(function() {
+  addTab();
+  $dialog.dialog('close');
+  return false;
+ });
+
+  // actual addTab function: adds new tab using the title input from the form above
+  function addTab() {
+   tab_title = $tab_title_input.val(); // groupName
+   var arysel  = []; // An array for the monitorIds
+   $("#selMonitors :selected").each(function(i, selected) { // For each selected MonitorId
+    arysel[i] = $(selected).val(); // Put it into the array
+   });
+   var mids = arysel.toString(); // Make a comman-separated list of the select MonitorsIds
+   $.post("skins/new/includes/updateGroups.php?groupName=" + tab_title + "&mids=" + mids + "&action=insert"); // Add the new Group
+   $("#tabs").tabs('add', 'skins/new/views/monitors.php?groupName='+tab_title, tab_title); // Add the actual tab
+  }
+
+  // addTab button: just opens the dialog
+  $('#add_tab').button().click(function() {
+   $dialog.dialog('open');
+  });
+
+  // close icon: removing the tab on click
+  // note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
+  $('#tabs span.ui-icon').click(function() {
+   var index = $('li',$("#tabs")).index($(this).parent());
+   $.post("skins/new/includes/updateGroups.php?action=delete&groupName=" + $(this).parent().find('a').text());
+   $("#tabs").tabs('remove', index);
+  });
+ } // END ADD_TAB_CLICK() //
 
   setInterval(function() {
    $("#monitors li").each(function() {
@@ -101,9 +118,5 @@
   });
  }, refresh);
 
-  $("#monitors").sortable({ opacity: 0.6, cursor: 'move', update: function() {
-  var order = $(this).sortable("serialize") + '&action=sequence';
-    $.post("skins/new/includes/updateSequence.php", order);
-   }});
 
  });
