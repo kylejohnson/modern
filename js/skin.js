a7c33feefd5f5822ec9a46b1af3e9b616e21ad19
Kyle Johnson <kjohnson@gnulnx.net>
2011-12-27 15:00:38 -0500

    Updated for ZM 1.25.0 release

--- js/skin.js
+++ js/skin.js
@@ -1,5 +1,5 @@
 //
-// ZoneMinder base static javascript file, $Date: 2009-05-08 11:04:12 +0100 (Fri, 08 May 2009) $, $Revision: 2865 $
+// ZoneMinder base static javascript file, $Date: 2011-06-21 10:19:10 +0100 (Tue, 21 Jun 2011) $, $Revision: 3459 $
 // Copyright (C) 2001-2008 Philip Coombes
 //
 // This program is free software; you can redistribute it and/or
@@ -21,21 +21,6 @@
 // This file should only contain static JavaScript and no php.
 // Use skin.js.php for JavaScript that need pre-processing
 //
-
-if ( !window.console )
-{
-    window.console =
-    {
-        init:function() {},
-        log:function() {},
-        debug:function() {},
-        info:function() {},
-        warn:function() {},
-        error:function() {}
-    };
-}
-if (! window.console.debug )//IE8 has console but doesn't have console.debug so lets alias it.
-    window.console.debug = console.log;
 
 // Javascript window sizes
 var popupSizes = {
@@ -60,6 +45,7 @@ var popupSizes = {
     'group':        { 'width': 360, 'height': 180 },
     'groups':       { 'width': 400, 'height': 220 },
     'image':        { 'addWidth': 48, 'addHeight': 80 },
+    'log':          { 'width': 980, 'height': 720 },
     'login':        { 'width': 720, 'height': 480 },
     'logout':       { 'width': 240, 'height': 100 },
     'monitor':      { 'width': 380, 'height': 364 },
@@ -67,15 +53,15 @@ var popupSizes = {
     'monitorprobe': { 'width': 500, 'height': 240 },
     'monitorselect':{ 'width': 160, 'height': 200 },
     'montage':      { 'width': -1, 'height': -1 },
-    'optionhelp':   { 'width': 320, 'height': 330 },
-    'options':      { 'width': 840, 'height': 620 },
+    'optionhelp':   { 'width': 400, 'height': 320 },
+    'options':      { 'width': 920, 'height': 620 },
     'preset':       { 'width': 300, 'height': 120 },
     'settings':     { 'width': 200, 'height': 225 },
     'state':        { 'width': 240, 'height': 124 },
     'stats':        { 'width': 740, 'height': 200 },
     'timeline':     { 'width': 760, 'height': 540 },
     'user':         { 'width': 320, 'height': 420 },
-    'version':      { 'width': 320, 'height': 140 },
+    'version':      { 'width': 360, 'height': 140 },
     'video':        { 'width': 420, 'height': 360 },
     'videoview':    { 'addWidth': 48, 'addHeight': 80 },
     'watch':        { 'addWidth': 96, 'minWidth': 420, 'addHeight': 384 },
@@ -91,22 +77,9 @@ function newWindow( url, name, width, height )
     var windowId = window.open( url, name, popupOptions+",width="+width+",height="+height );
 }
 
-function clone( obj )
-{
-    if ( typeof(obj) != 'object' )
-        return( obj );
-    if ( obj == null )
-        return( obj );
-
-    var newObj = new Object();
-    for ( var i in obj )
-        newObj[i] = clone( obj[i] );
-    return( newObj );
-}
-
 function getPopupSize( tag, width, height )
 {
-    var popupSize = clone( popupSizes[tag] );
+    var popupSize = Object.clone( popupSizes[tag] );
     if ( !popupSize )
     {
         console.error( "Can't find window size for tag '"+tag+"'" );
